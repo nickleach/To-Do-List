bc12fe85597364bfc03fa3be117b029c98089697
Nick Leach <nickleach22@gmail.com>
2015-06-04 15:05:24 -0400

    Finished class example

--- app/js/main.js
+++ app/js/main.js
@@ -10,18 +10,31 @@
 var storageBin = [];
 
 // var a = new Todo({task: taskText});
+
 //Set up submit
 //on submit create a new instance
+
 $('#addTask').on('submit', function(event){
   event.preventDefault();
   var taskText = $('#taskText').val();
   var taskInstance = new Todo({task: taskText});
   storageBin.push(taskInstance);
   $('#tasks').append('<li>' + taskText + '</li>');
+  this.reset();
 });
 
 // toggle item
-$('#tasks').on('click', 'li' , function(){
-  console.log('funny guy');
+
+$('#tasks').on('click', 'li', function(){
+  event.preventDefault();
+
+  //grab the list item I clicked on
+  //mark that item as completed
+  $(this).addClass('complete');
+  var tTask= $(this).text();
+  var taskToEdit = _.find(storageBin, { task: tTask });
+  taskToEdit.status = 'Closed';
+  console.log(taskToEdit);
 });
+
 // }());

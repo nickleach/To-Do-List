99c3479dfb344a02e769fbf27c16db74d76f9824
Nick Leach <nickleach22@gmail.com>
2015-06-04 14:47:27 -0400

    Class Example

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,27 @@
-console.log('This is cool!');
+// (function(){
 
-$('.container').html(template.example({
-  value: 'Hello, World!!'
-}));
+  var Todo = function(options){
+  var args = options || {};
+
+  this.task = options.task;
+  this.status = 'Open';
+
+};
+var storageBin = [];
+
+// var a = new Todo({task: taskText});
+//Set up submit
+//on submit create a new instance
+$('#addTask').on('submit', function(event){
+  event.preventDefault();
+  var taskText = $('#taskText').val();
+  var taskInstance = new Todo({task: taskText});
+  storageBin.push(taskInstance);
+  $('#tasks').append('<li>' + taskText + '</li>');
+});
+
+// toggle item
+$('#tasks').on('click', 'li' , function(){
+  console.log('funny guy');
+});
+// }());

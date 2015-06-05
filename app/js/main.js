8d7844dad565c32d07abf6d927632f732ab47f7a
Nick Leach <nickleach22@gmail.com>
2015-06-05 11:01:33 -0400

    Added second ul

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,23 @@
 // (function(){
 
   var Todo = function(options){
-  var args = options || {};
+    var args = options || {};
 
-  this.task = options.task;
-  this.status = 'Open';
+    this.task = options.task;
+    this.status = 'Open';
 
-};
-var storageBin = [];
+  };
+  // var Completed = function(options){
+  //   var com = options || {};
+
+  //   this.task = options.task;
+  //   // this.status = 'Closed';
+  // }
+
+
+  var storageBin = [];
+  var completedTask = [];
+
 
 // var a = new Todo({task: taskText});
 
@@ -19,29 +29,45 @@ $('#addTask').on('submit', function(event){
   var taskText = $('#taskText').val();
   var taskInstance = new Todo({task: taskText});
   storageBin.push(taskInstance);
-  // $('#tasks').append('<li>' + taskText + '</li>');
-  $('#tasks').html(template.example({value:storageBin}));
+  $('#tasks').append('<li><input type="checkbox">' + taskText + '<span class= "fa fa-minus-square"</span></li>');
+  // $('#tasks').html(template.example({value:storageBin}));
   this.reset();
 });
 //Reset Button
 $('.fa-undo').on('click', function(event){
   event.preventDefault();
   $('#tasks').html('');
-  storageBin = [];
+
+  $('.complete').html('');
+   storageBin = [];
 
 });
 // toggle item
 
-$('#tasks').on('click', 'li', function(){
+$('.tasks').on('click', 'li', function(){
   event.preventDefault();
-
-  //grab the list item I clicked on
-  //mark that item as completed
   $(this).addClass('complete');
   var tTask= $(this).text();
+  $('.complete').append('<li><input type="checkbox">'+ tTask + '<span class= "fa fa-minus-square"</span></li>');
   var taskToEdit = _.find(storageBin, { task: tTask });
   taskToEdit.status = 'Closed';
-  console.log(taskToEdit);
-});
+  $(this).remove();
 
+});
+$('.complete').on('click', 'li', function(){
+  event.preventDefault();
+  $(this).removeClass('complete');
+  var tTask= $(this).text();
+  $('.tasks').append('<li><input type="checkbox">'+ tTask + '<span class= "fa fa-minus-square"</span></li>');
+  var taskToEdit = _.find(storageBin, { task: tTask });
+  taskToEdit.status = 'Open';
+  $(this).remove();
+
+});
+$('.fa-minus-square').on('click', 'li',function(){
+  var tTask= $(this).text().remove();
+  var taskToEdit = _.find(storageBin, { task: tTask });
+
+
+});
 // }());
